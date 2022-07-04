980493050ce77cd74d69226fdcee3b1f6e38b98d
Ale Centalz <alejandra.centeno.alcaraz@ibm.com>
2022-07-04 10:00:00 -0500

    add pipes app

---
+++ 04-pipesApp/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string = "Ale cenTeno";
+  value: number = 1000;
+
+  showName() {
+    console.log(this.name);
+  }
+}
